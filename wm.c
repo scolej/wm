927d99c234292a14c4f50264b46cd6fe9ee367a2
sjc <s.cole.j@gmail.com>
2021-08-22 21:48:53 +1000

    click position sensitive move/resize

--- wm.c
+++ wm.c
@@ -26,6 +26,7 @@ void fine(char* msg, ...) {
   // todo
 }
 
+// todo pass around a context?
 Display* dsp;
 XColor red, grey;
 
@@ -51,28 +52,96 @@ void handle_enter_notify(XCrossingEvent* event) {
   XSetInputFocus(dsp, win, RevertToParent, t);
 }
 
+enum DragKind {
+  MOVE,
+  RESIZE_E, RESIZE_W, RESIZE_N, RESIZE_S,
+  RESIZE_NW, RESIZE_NE, RESIZE_SW, RESIZE_SE,
+};
+
+enum DragHandle {
+  LOW, MIDDLE, HIGH
+};
+
 struct {
   Window win;
   int start_mouse_x;
   int start_mouse_y;
   int start_win_x;
   int start_win_y;
+  int start_win_w;
+  int start_win_h;
+  enum DragKind kind;
 } drag_state;
 
 void handle_button_press(XButtonEvent* event) {
   Window win = event->subwindow;
-  info("started dragging window %d", win);
-
   int x = event->x_root;
   int y = event->y_root;
-  drag_state.win = win;
-  drag_state.start_mouse_x = x;
-  drag_state.start_mouse_y = y;
 
   XWindowAttributes attr;
   XGetWindowAttributes(dsp, win, &attr);
-  drag_state.start_win_x = attr.x;
-  drag_state.start_win_y = attr.y;
+  int win_x = attr.x;
+  int win_y = attr.y;
+  int win_w = attr.width;
+  int win_h = attr.height;
+
+  float r = 0.1;
+  int x1, x2, y1, y2;
+  x1 = win_x + win_w * r;
+  x2 = win_x + win_w * (1.0 - r);
+  y1 = win_y + win_h * r;
+  y2 = win_y + win_h * (1.0 - r);
+  info("drag thresholds are %d %d and %d %d", x1, x2, y1, y2);
+
+  enum DragHandle dhx;
+  if (x < x1) {
+    dhx = LOW;
+  } else if (x < x2) {
+    dhx = MIDDLE;
+  } else {
+    dhx = HIGH;
+  }
+
+  enum DragHandle dhy;
+  if (y < y1) {
+    dhy = LOW;
+  } else if (y < y2) {
+    dhy = MIDDLE;
+  } else {
+    dhy = HIGH;
+  }
+
+  enum DragKind dk;
+  if (dhx == LOW && dhy == MIDDLE) {
+    dk = RESIZE_W;
+  } else if (dhx == HIGH && dhy == MIDDLE) {
+    dk = RESIZE_E;
+  } else if (dhx == MIDDLE && dhy == LOW) {
+    dk = RESIZE_N;
+  } else if (dhx == MIDDLE && dhy == HIGH) {
+    dk = RESIZE_S;
+  } else if (dhx == LOW && dhy == LOW) {
+    dk = RESIZE_NW;
+  } else if (dhx == HIGH && dhy == LOW) {
+    dk = RESIZE_NE;
+  } else if (dhx == LOW && dhy == HIGH) {
+    dk = RESIZE_SW;
+  } else if (dhx == HIGH && dhy == HIGH) {
+    dk = RESIZE_SE;
+  } else {
+    dk = MOVE;
+  }
+
+  drag_state.win = win;
+  drag_state.start_win_x = win_x;
+  drag_state.start_win_y = win_y;
+  drag_state.start_win_w = win_w;
+  drag_state.start_win_h = win_h;
+  drag_state.start_mouse_x = x;
+  drag_state.start_mouse_y = y;
+  drag_state.kind = dk;
+
+  info("started dragging window %d", win);
 }
 
 void handle_button_release(XButtonEvent* event) {
@@ -85,10 +154,82 @@ void handle_motion(XMotionEvent* event) {
     return;
   }
 
-  int x = event->x_root - drag_state.start_mouse_x + drag_state.start_win_x;
-  int y = event->y_root - drag_state.start_mouse_y + drag_state.start_win_y;
-  fine("dragging window %d to %d %d", drag_state.win, x, y);
-  XMoveWindow(dsp, drag_state.win, x, y);
+  int ex = event->x_root;
+  int ey = event->y_root;
+  int sx = drag_state.start_mouse_x;
+  int sy = drag_state.start_mouse_y;
+  int wx = drag_state.start_win_x;
+  int wy = drag_state.start_win_y;
+  int ww = drag_state.start_win_w;
+  int wh = drag_state.start_win_h;
+
+  int dx = ex - sx;
+  int dy = ey - sy;
+
+  // todo maybe this switch would be better over at drag start
+  int xp, xw, yp, yw;
+  switch (drag_state.kind) {
+  case MOVE:
+    xp = 1;
+    yp = 1;
+    xw = 0;
+    yw = 0;
+    break;
+  case RESIZE_E:
+    xp = 0;
+    yp = 0;
+    xw = 1;
+    yw = 0;
+    break;
+  case RESIZE_W:
+    xp = 1;
+    yp = 0;
+    xw = -1;
+    yw = 0;
+    break;
+  case RESIZE_N:
+    xp = 0;
+    yp = 1;
+    xw = 0;
+    yw = -1;
+    break;
+  case RESIZE_S:
+    xp = 0;
+    yp = 0;
+    xw = 0;
+    yw = 1;
+    break;
+  case RESIZE_NW:
+    xp = 1;
+    yp = 1;
+    xw = -1;
+    yw = -1;
+    break;
+  case RESIZE_NE:
+    xp = 0;
+    yp = 1;
+    xw = 1;
+    yw = -1;
+    break;
+  case RESIZE_SW:
+    xp = 1;
+    yp = 0;
+    xw = -1;
+    yw = 1;
+    break;
+  case RESIZE_SE:
+    xp = 0;
+    yp = 0;
+    xw = 1;
+    yw = 1;
+    break;
+  }
+
+  int x = wx + xp * dx;
+  int y = wy + yp * dy;
+  int w = ww + xw * dx;
+  int h = wh + yw * dy;
+  XMoveResizeWindow(dsp, drag_state.win, x, y, w, h);
 }
 
 void handle_focus_in(XFocusChangeEvent* event) {
